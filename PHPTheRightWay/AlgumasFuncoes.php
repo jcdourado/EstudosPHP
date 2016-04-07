3e5a3af5d249be59df22bf287e0021ee66b858de
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-04-07 09:52:40 -0300

    Commit algumas funcoes

---
+++ PHPTheRightWay/AlgumasFuncoes.php
@@ -0,0 +1,8 @@
+<?php
+  // String -> strlen, str_pad, str_replace, str_repeat, substr, strrev, strstr, strcasecmp, ucwords, strpos
+  // Array -> count, array_shift, array_unshit, sort (sem indice), asort (com indice), rsort, array_push, array_slice, array_pop
+  // Xml -> simplexml_load_file -> children()
+  // stdClass -> classe para colocar atributos
+  // parse_ini_file -> pegar um ini e transformar em array
+  // move_uploaded_file -> mover arquivos
+ ?>
