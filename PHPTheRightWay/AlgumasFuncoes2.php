dc0f5c7e77f24b3f7f260fb24fc6362bba3349bc
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-04-07 13:12:38 -0300

    COmmit inicio XDebug

---
+++ PHPTheRightWay/AlgumasFuncoes2.php
@@ -0,0 +1,8 @@
+<?php
+  // Biblioteca SPL -> padrão no PHP, possui diversas classes, interfaces para auxilio com estruturas de dados
+
+  // php -a -> roda ele interativamente, também serve para scripts locais
+    // variaveis: $argv (array com os valores) e $argc (quantos), exit(1) -> exit
+    // php "arquivos" -> executa ele na linha de comandos
+
+ ?>
