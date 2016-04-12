00a3d6e42f7d2fe25c0eb92042fef4e72b066111
Julio Cezar Dourado <jcd17071997@hotmail.com>
2016-04-12 11:13:55 -0300

    Commit

--- PHPTheRightWay/AlgumasFuncoes2.php
+++ PHPTheRightWay/AlgumasFuncoes2.php
@@ -7,6 +7,22 @@
 
   // XDebuger -> debugador para diversas IDE's
 
-  
+  // Para trabalhar com Datas e Horarios - Classe: DateTime:
+      // Metodos: createFromFormat, format, classe: DateInterval, add, sub
+
+  // Para trabalhar com UTF-8:
+    // Tem que se usar as funcoes que começam com mb_
+    // mb_internal_encoding -> iniciar a codificacao em UTF-8
+    // mb_http_output -> mandar para o cliente
+
+  // PDO -> tem metodo prepare para usar o '?' e o bindValue para alterar a posição
+    // query, statement, fetch
+
+  // password_hash (cira um hash que é irrevesivel), password_verify
+
+
+  // Testes: PHPUnit, Selenium
+  // Testes por BDD(Comportamento): Behat, PHPSpec
+
 
  ?>
